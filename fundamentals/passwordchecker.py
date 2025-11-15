01cf3ed26bf3cf0313b7d6c038f1b7a66e919efb
sen <shend1985144@proton.me>
2025-11-15 08:32:42 +0700

    passwordchecker modified

--- fundamentals/passwordchecker.py
+++ fundamentals/passwordchecker.py
@@ -1,31 +1,39 @@
-password = input("Masukakn passwor mu: ")
-x = 0
-if any(char.islower() for char in password):
-    x += 1
+password = input("Input your password: ")
+def passwordChecker(password):
+    global x
+    x = 0
+    if any(char.islower() for char in password):
+        x += 1
 
-if any(char.isupper() for char in password):
-    x += 1
+    if any(char.isupper() for char in password):
+        x += 1
 
-if any(char.isdigit() for char in password):
-    x += 1
+    if any(char.isdigit() for char in password):
+        x += 1
 
-if any(not char.isalnum() for char in password):
-    x += 1
+    if any(not char.isalnum() for char in password):
+        x += 1
 
-if len(password) >= 8:
-    x += x
+    if len(password) >= 8:
+        x += 1
 
-if x == 1:
-    print("Butut")
-elif x == 2:
-    print("lumayan butut")
-elif x == 3:
-    print("lumayan alus")
-elif x == 4:
-    print("alus")
-elif x == 5:
-    print("alus pisan")
-elif x == 0:
-    print("Babarian jebol")
+    if len(password) < 8:
+        x -= x
 
-print(f"'{password}' is level {x}")
+    print(f"Password level: {x} |",end=" ")
+
+    if x == 1:
+        print("very bad")
+    elif x == 2:
+        print("bad")
+    elif x == 3:
+        print("maybe good")
+    elif x == 4:
+        print("good")
+    elif x == 5:
+        print("strong")
+    elif x == 0:
+        print("wtf are you doing?? your password need to be 8 digit!!")
+
+   
+passwordChecker(password)
